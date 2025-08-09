5e15f6235f8ec08af6aa8ce66d238c88a6de4bf9
derrick <derrick.mwangi2@student.moringaschool.com>
2025-08-09 19:36:40 +0300

    Complete Anagram assignment and update previous files

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, candidate_list):
+        results = []
+        for candidate in candidate_list:
+            # skip identical word
+            if candidate.lower() != self.word and sorted(candidate.lower()) == sorted(self.word):
+                results.append(candidate)
+        return results
